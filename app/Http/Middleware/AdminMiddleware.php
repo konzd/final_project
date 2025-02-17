0b7e11df805d3e084ae7b4063e4224782564c80a
konzd <m4761457@gmail.com>
2025-02-17 11:31:14 +0700

    main

--- app/Http/Middleware/AdminMiddleware.php
+++ app/Http/Middleware/AdminMiddleware.php
@@ -5,18 +5,23 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 
 class AdminMiddleware
 {
     public function handle(Request $request, Closure $next)
-    {
-        if (Auth::user() && Auth::user()->role === 'admin') {
-            return $next($request);
-        }
+{
+    $user = Auth::guard('api')->user();
+    Log::info('User in Middleware:', [$user]);
 
-        return response()->json([
-            'success' => false,
-            'message' => 'Access denied. Admins only.'
-        ], 403);
+    if ($user && $user->role === 'admin') {
+        return $next($request);
     }
+
+    return response()->json([
+        'success' => false,
+        'message' => 'Access denied. Admins only.'
+    ], 403);
+}
+
 }
